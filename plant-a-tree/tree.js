667d8afae19c5c50531b32b8d112b920f96bb2f8
Karen Lin <karenarialin@gmail.com>
2017-04-08 15:54:04 -0700

    New changes, publishing tree project

--- plant-a-tree/tree.js
+++ plant-a-tree/tree.js
@@ -25,22 +25,62 @@ $('.place-section').waypoint(function(direction) {
         $('.place-section').addClass('place-animate'); 
 }, { offset: '50%'});
     
-
-$('.fill').waypoint(function(direction) {
-        $('.fill').addClass('animate-fill'); 
-}, { offset: '90%'});
-    
 $('.water').waypoint(function(direction) {
         $('.water').addClass('animate-fill'); 
 }, { offset: '90%'});
+    
+$('.what').waypoint(function(direction) {
+        $('.what').addClass('animate-fill'); 
+}, { offset: '80%'});
+    
+$('.where').waypoint(function(direction) {
+        $('.where').addClass('animate-fill'); 
+}, { offset: '80%'});
+    
+$('.when').waypoint(function(direction) {
+        $('.when').addClass('animate-fill'); 
+}, { offset: '80%'});
+    
+$('.why').waypoint(function(direction) {
+        $('.why').addClass('animate-fill'); 
+}, { offset: '80%'});
     
 $('.stake').waypoint(function(direction) {
         $('.stake').addClass('animate-stake');   
 }, { offset: '50%'});
     
-$('.fill-page').waypoint(function(direction) {
+$('.fill-shovel').waypoint(function(direction) {
     $('.double-shovel-dig').addClass('dig-animation');   
-}, { offset: '50%'});
+}, { offset: '90%'});
+    
+$('.fill').waypoint(function(direction) {
+        $('.fill').addClass('animate-fill'); 
+}, { offset: '90%'});
+    
+$('.measure-page').waypoint(function(direction) {
+        $('.measure-page').addClass('animate-fill'); 
+}, { offset: '90%'});
+    
+$('.dig-page').waypoint(function(direction) {
+        $('.dig-page').addClass('animate-fill'); 
+}, { offset: '90%'});
+    
+$('.place-page').waypoint(function(direction) {
+        $('.place-page').addClass('animate-fill'); 
+}, { offset: '90%'});
+    
+$('.fill-page').waypoint(function(direction) {
+        $('.fill-page').addClass('animate-fill'); 
+}, { offset: '90%'});
+    
+    
+$('.stake-page').waypoint(function(direction) {
+        $('.stake-page').addClass('animate-fill'); 
+}, { offset: '90%'});
+    
+$('.water-page').waypoint(function(direction) {
+        $('.water-page').addClass('animate-fill'); 
+}, { offset: '90%'});    
     
     //main closing bracket
 }
